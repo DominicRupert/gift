8e8dfb83f30b073110655122ca4b228af402b881
big-daddy-dom <dominicrupert@gmail.com>
2022-04-14 13:41:59 -0600

    first commit

---
+++ app/Services/AxiosService.js
@@ -0,0 +1,15 @@
+
+// @ts-ignore
+export const giphyApi = axios.create({
+    baseURL: "http://api.giphy.com/v1/gifs",
+    timeout: 8000
+  })
+
+
+
+Mz7FrQZfs574Ija8b3viGcfCgtGxTwhB
+// @ts-ignore
+export const sandboxApi = axios.create({
+    baseURL: "https://bcw-sandbox.herokuapp.com/api/gifts",
+    timeout: 8000
+  })
