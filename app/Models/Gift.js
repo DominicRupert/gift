df5b9a7065bfea981f48bfe5bf067f45adc0990d
big-daddy-dom <dominicrupert@gmail.com>
2022-04-14 15:21:34 -0600

    cow

--- app/Models/Gift.js
+++ app/Models/Gift.js
@@ -1,13 +1,13 @@
 export class Gift{
     constructor(data){
         this.tag = data.title
-        this.url = data.url
+        this.url = data.images.downsized_medium.url
     }
     get Template(){
         return /*html*/`
         <div>
         <h1>${this.tag}</h1>
-        <img src="('${this.url}')" alt="">
+        <img src='${this.url}' alt="">
         </div>
         `
     }
