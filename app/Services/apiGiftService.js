cebe25026b63cd1540f92f5f6fd75cc1410dc113
Dylan Cade <dacade.code@gmail.com>
2022-04-14 14:37:05 -0600

    ops

--- app/Services/apiGiftService.js
+++ app/Services/apiGiftService.js
@@ -8,19 +8,11 @@ let params ={
     q: ''
 }
 class  ApiGiftService{
-<<<<<<< HEAD
     async searchGiphy(query){
         params.q = query
         const res = await giphyApi.get('search', { params })
         console.log(res)
         // const gift = new Gift()
-=======
-    async searchGiphy(){
-        const res = await giphyApi.get('/search?api_key=Mz7FrQZfs574Ija8b3viGcfCgtGxTwhB&q=&rating=pg&lang=en')
-        const gift = new Gift(res.data)
-
->>>>>>> 0d63118457af497892acce2f8cef6efc16d376ff
-    }
-    
+    } 
 }
 export const apiGiftService = new ApiGiftService();
