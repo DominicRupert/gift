0d63118457af497892acce2f8cef6efc16d376ff
big-daddy-dom <dominicrupert@gmail.com>
2022-04-14 14:25:27 -0600

    model

---
+++ app/Models/Gift.js
@@ -0,0 +1,13 @@
+export class Gift{
+    constructor(data){
+        this.tag = data.tag
+        this.url = data.url
+    }
+    get Template(){
+        return /*html*/`
+        <div>
+        <h1>${this.tag}</h1>
+        <img src="('${this.url}')" alt="">
+        </div>
+    }
+}
